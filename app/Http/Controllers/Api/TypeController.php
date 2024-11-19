7d9194f9439c5ea9a47c289b95936bf4cb7f6af1
AndreaMargutti <marguttiandrea30@gmail.com>
2024-11-19 21:45:56 +0100

    add route for types api

--- app/Http/Controllers/Api/TypeController.php
+++ app/Http/Controllers/Api/TypeController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Type;
 use Illuminate\Http\Request;
 
 class TypeController extends Controller
 {
     //
     public function index() {
-
+        $types = Type::all();
+        return response()->json([
+            "success" => true,
+            "results" => $types,
+        ]);
     }
 }
